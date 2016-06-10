037112c4a601caabe5f65bdd2298e431c2b9ce13
siddharth51292 <siddharth.avadhanam@gmail.com>
2016-06-10 18:50:39 -0400

    simpler GWAS1 script

--- GWASone.R
+++ GWASone.R
@@ -8,10 +8,8 @@ GWASone <- function(geno,pheno,cov,i){
         sumfit <- vector() 
         logp <- vector()
         
-        mod<- model.matrix(~as.factor(w[,1])+ as.factor(w[,2]))
-        
     
-        fit <- lm(y~X[,i]+mod[,2]+mod[,3]+mod[,4]+mod[,5]+mod[,6]+mod[,7]+mod[,8]+mod[,9]) ## y ~ X[,i] + (confounders)
+        fit <- lm(y~X[,i]+as.factor(w[,1])+as.factor(w[,2])) ## y ~ X[,i] + (confounders)
         sumfit <- summary(fit)$coefficients  
         
         
